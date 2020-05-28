bb91282278912bf5b1e399bf8c78dbb2d34fea24
ishwar arya <ishwar.arya089@gmail.com>
2020-05-28 07:07:43 +0530

    mapping corrected

--- src/main/java/com/example/spring/mongo/controller/BookStoreController.java
+++ src/main/java/com/example/spring/mongo/controller/BookStoreController.java
@@ -5,14 +5,20 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.spring.mongo.model.Book;
 import com.example.spring.mongo.service.BookService;
-
+/**
+ * book store request handler
+ * @author ishu1010	
+ */
 @RestController
 public class BookStoreController {
 	
@@ -26,7 +32,7 @@ public class BookStoreController {
 	@GetMapping("/book/{id}")
 	public ResponseEntity<Book> getbook(@PathVariable Integer id){
 		 Optional<Book> bookOp= bookService.getBook(id);
-		 bookOp.orElseThrow(NullPointerException :: new);
+		 bookOp.orElseThrow(IllegalArgumentException :: new);
 		return ResponseEntity.status(HttpStatus.OK).body(bookOp.get());
 	}
 
@@ -37,19 +43,19 @@ public class BookStoreController {
 	}
 
 	
-	@GetMapping("/book/save-book")
+	@PostMapping("/book/save-book")
 	public ResponseEntity<Book> saveBook(@RequestBody Book requestBook){
 		 Book book = bookService.saveBook(requestBook);
 		return ResponseEntity.status(HttpStatus.OK).body(book);
 	}
 	
-	@GetMapping("/book/update-book")
+	@PutMapping("/book/update-book")
 	public ResponseEntity<Book> updateBook(@RequestBody Book requestBook){
-		 Book book = bookService.saveBook(requestBook);
+		 Book book = bookService.updateBook(requestBook);
 		return ResponseEntity.status(HttpStatus.OK).body(book);
 	}
 	
-	@GetMapping("/book/delete-book")
+	@DeleteMapping("/book/delete-book")
 	public ResponseEntity<String> deleteBook(@RequestBody Book requestBook){
 		 bookService.deleteBook(requestBook);
 		return ResponseEntity.status(HttpStatus.OK).body("done!");
